0df31f1a8c89ea22d8795482a2d7263cc337a40b
jslim7812 <60832506+jslim7812@users.noreply.github.com>
2020-05-20 13:12:09 +0900

    Add files via upload

---
+++ 200520-amazon.py
@@ -0,0 +1,26 @@
+import requests
+import re
+import os
+
+# http://www.useragentstring.com/ 의 내용 복사
+headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36'}
+
+os.chdir('./3')
+
+with open("./yogi_listing2.csv") as f:
+    url_csv= f.readlines()
+   
+i = 1
+for url in url_csv:
+    resp = requests.get(url, headers = headers)
+    soup = resp.text
+    ama = re.findall('hiRes":".*?",', soup)
+    print("URL","(",i, "/", len(url_csv),")", url)
+    i = i+1
+
+    for j in range(len(ama)):
+        img = ama[j][8:-2]
+        print(" [", j+1, "/", len(ama), "] ", img)
+    
+    print()
+    print()
