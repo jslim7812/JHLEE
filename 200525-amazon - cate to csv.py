3fff478559277aa8ca59549c7cc760d154d6f989
jslim7812 <60832506+jslim7812@users.noreply.github.com>
2020-05-25 11:19:34 +0900

    Update 200525-amazon - cate to csv.py

--- 200525-amazon - cate to csv.py
+++ 200525-amazon - cate to csv.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import os
+import time
 
 # http://www.useragentstring.com/ 의 내용 복사
 headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36'}
@@ -19,6 +20,7 @@ for url in url_csv:
     soup = BeautifulSoup(resp.text, features='lxml')
     cate = soup.find_all("a", class_ = 'a-link-normal a-color-tertiary')
     cate_list=[]
+    time.sleep(0.5)
     for i in range(0,len(cate)):
         a=cate[i].get_text(" ", strip=True)
         cate_list.append(a)
