969611b0de5e35ca0efa78275fdd0fc095989b88
jslim7812 <60832506+jslim7812@users.noreply.github.com>
2021-12-22 10:23:32 +0900

    Add files via upload

---
+++ 211222-amazon (ratio, no1image).py
@@ -0,0 +1,65 @@
+import requests
+import re
+import glob, os, shutil
+import time
+import random
+from PIL import Image
+
+# http://www.useragentstring.com/ 의 내용 복사
+headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36'}
+
+os.chdir(r'C:/pyml-master/3') #요기
+
+with open("./barbie.csv") as f: #요기
+    url_csv= f.readlines()
+
+os.makedirs(r'C:/pyml-master/3/image/resize/1') #요기
+os.chdir(r'C:/pyml-master/3/image') #요기
+
+i = 1
+for url in url_csv:
+    while True:
+        resp = requests.get(url, headers = headers, timeout = 2).text
+        #soup = resp.text
+        ama = re.findall('{"hiRes":"h.*?",', resp)
+        print("URL","(",i, "/", len(url_csv),")", url, "(",len(ama),")")
+        if len(ama) != 0:
+            #break
+
+            for j in range(len(ama)):
+                img = ama[j][10:-2]
+                r = requests.get(img)
+                file = open("{0:04}-".format(i)+"{0:04}.jpg".format(j+1),"wb")
+                file.write(r.content)
+                file.close()
+                filename = str(file)[26:-2]
+                image = Image.open(filename)
+                if image.size[0] <= image.size[1]:
+                    height = 1000
+                    width = int(image.size[0]/image.size[1]*1000)
+                else:
+                    width = 1000
+                    height = int(image.size[1]/image.size[0]*1000)
+                nsize = (width, height)
+                print(nsize)
+
+                resize_image = image.resize(nsize)
+                background = Image.new('RGB', (1000, 1000), (255, 255, 255, 255))
+                offset = (int(round(((1000 - width) / 2), 0)), int(round(((1000 - height) / 2),0)))
+                background.paste(resize_image, offset)
+                background.save('./resize/'+filename)
+                print(" [", j+1, "/", len(ama), "] ", img, "==>>", file)
+            break
+
+    i = i+1
+    time.sleep( random.uniform(1,3) )
+
+    
+    print()
+    print()
+
+#대표이미지 '/1'폴더로 복사
+files = glob.iglob(os.path.join('./resize/', "*01.jpg"))
+for file in files:
+    if os.path.isfile(file):
+        shutil.copy2(file, './resize/1')
