a8866c3903aa2a3fc78179ef1c243511ae182c73
jslim7812 <60832506+jslim7812@users.noreply.github.com>
2022-01-28 18:26:45 +0900

    Update 211222-amazon (ratio, no1image).py

--- 211222-amazon (ratio, no1image).py
+++ 211222-amazon (ratio, no1image).py
@@ -4,25 +4,36 @@ import glob, os, shutil
 import time
 import random
 from PIL import Image
+from selenium import webdriver
+from bs4 import BeautifulSoup
 
 # http://www.useragentstring.com/ 의 내용 복사
 headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36'}
 
 os.chdir(r'C:/pyml-master/3') #요기
 
-with open("./barbie.csv") as f: #요기
+with open("./ravensburger2.csv") as f: #요기
     url_csv= f.readlines()
 
-os.makedirs(r'C:/pyml-master/3/image/resize/1') #요기
+#os.makedirs(r'C:/pyml-master/3/image/resize/1') #요기
 os.chdir(r'C:/pyml-master/3/image') #요기
+path = "c:/crawling/chromedriver.exe"
 
-i = 1
-for url in url_csv:
+
+i = 36
+for url in url_csv[i-1:]:
     while True:
-        resp = requests.get(url, headers = headers, timeout = 2).text
+        wd = webdriver.Chrome(path)
+        wd.get(url)
+        html = wd.page_source
+        bs = BeautifulSoup(html, 'html.parser').get_text()
+        ama = re.findall('{"hiRes":"h.*?",', bs)
+        wd.close()
+        #resp = requests.get(url, headers = headers, timeout = 2).text
         #soup = resp.text
-        ama = re.findall('{"hiRes":"h.*?",', resp)
+        #ama = re.findall('{"hiRes":"h.*?",', resp)
         print("URL","(",i, "/", len(url_csv),")", url, "(",len(ama),")")
+        time.sleep( random.uniform(0,1))
         if len(ama) != 0:
             #break
 
